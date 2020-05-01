511db7680022a6b1afc12d0f8840bd127a8a448d
Jeff Scott <jpscott@ranken.edu>
2020-05-01 10:36:52 -0500

    initial commit

---
+++ BMIGUI/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace BMIGUI
+{
+    partial class FormBMI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelHeight = new System.Windows.Forms.Label();
+            this.labelWeight = new System.Windows.Forms.Label();
+            this.labelBMI = new System.Windows.Forms.Label();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.textBoxWeight = new System.Windows.Forms.TextBox();
+            this.textBoxBMI = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxBMIStatus = new System.Windows.Forms.TextBox();
+            this.labelBMIStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeight.Location = new System.Drawing.Point(81, 34);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(100, 23);
+            this.labelHeight.TabIndex = 6;
+            this.labelHeight.Text = "Height:";
+            this.labelHeight.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelWeight
+            // 
+            this.labelWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelWeight.Location = new System.Drawing.Point(81, 89);
+            this.labelWeight.Name = "labelWeight";
+            this.labelWeight.Size = new System.Drawing.Size(100, 23);
+            this.labelWeight.TabIndex = 7;
+            this.labelWeight.Text = "Weight:";
+            this.labelWeight.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // labelBMI
+            // 
+            this.labelBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBMI.Location = new System.Drawing.Point(81, 147);
+            this.labelBMI.Name = "labelBMI";
+            this.labelBMI.Size = new System.Drawing.Size(100, 23);
+            this.labelBMI.TabIndex = 8;
+            this.labelBMI.Text = "BMI:";
+            this.labelBMI.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxHeight.Location = new System.Drawing.Point(187, 34);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(183, 26);
+            this.textBoxHeight.TabIndex = 0;
+            // 
+            // textBoxWeight
+            // 
+            this.textBoxWeight.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxWeight.Location = new System.Drawing.Point(187, 89);
+            this.textBoxWeight.Name = "textBoxWeight";
+            this.textBoxWeight.Size = new System.Drawing.Size(183, 26);
+            this.textBoxWeight.TabIndex = 1;
+            // 
+            // textBoxBMI
+            // 
+            this.textBoxBMI.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBoxBMI.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBMI.Location = new System.Drawing.Point(187, 147);
+            this.textBoxBMI.Name = "textBoxBMI";
+            this.textBoxBMI.ReadOnly = true;
+            this.textBoxBMI.Size = new System.Drawing.Size(183, 26);
+            this.textBoxBMI.TabIndex = 5;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(55, 273);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(95, 52);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonClear.Location = new System.Drawing.Point(172, 273);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(95, 52);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(288, 273);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(95, 52);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // textBoxBMIStatus
+            // 
+            this.textBoxBMIStatus.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBoxBMIStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBMIStatus.Location = new System.Drawing.Point(187, 198);
+            this.textBoxBMIStatus.Name = "textBoxBMIStatus";
+            this.textBoxBMIStatus.ReadOnly = true;
+            this.textBoxBMIStatus.Size = new System.Drawing.Size(183, 26);
+            this.textBoxBMIStatus.TabIndex = 9;
+            // 
+            // labelBMIStatus
+            // 
+            this.labelBMIStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBMIStatus.Location = new System.Drawing.Point(55, 198);
+            this.labelBMIStatus.Name = "labelBMIStatus";
+            this.labelBMIStatus.Size = new System.Drawing.Size(126, 23);
+            this.labelBMIStatus.TabIndex = 10;
+            this.labelBMIStatus.Text = "BMI Status:";
+            this.labelBMIStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // FormBMI
+            // 
+            this.AcceptButton = this.buttonCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.buttonClear;
+            this.ClientSize = new System.Drawing.Size(480, 365);
+            this.Controls.Add(this.textBoxBMIStatus);
+            this.Controls.Add(this.labelBMIStatus);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxBMI);
+            this.Controls.Add(this.textBoxWeight);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.labelBMI);
+            this.Controls.Add(this.labelWeight);
+            this.Controls.Add(this.labelHeight);
+            this.Name = "FormBMI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Body Mass Index (BMI) C# GUI Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelHeight;
+        private System.Windows.Forms.Label labelWeight;
+        private System.Windows.Forms.Label labelBMI;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.TextBox textBoxWeight;
+        private System.Windows.Forms.TextBox textBoxBMI;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxBMIStatus;
+        private System.Windows.Forms.Label labelBMIStatus;
+    }
+}
+
---
+++ BMIGUI/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMIGUI
+{
+    public partial class FormBMI : Form
+    {
+        public FormBMI()
+        {
+            InitializeComponent();
+        }
+
+        //  Declare and initialize global HEIGHT constants
+        const int MINHEIGHT = 12;   //  Minimum allowed height
+        const int MAXHEIGHT = 96;   //  Maximum allowed height
+        const int DEFHEIGHT = 72;   //  Default height
+
+        //  Declare and initialize global WEIGHT constants
+        const int MINWEIGHT = 1;    //  Minimum allowed weight
+        const int MAXWEIGHT = 777;  //  Maximum allowed wheight
+        const int DEFWEIGHT = 175;  //  Default weight
+
+        //  Declare and initialize global BMI constants
+        const double MINOPT = 18.5; //  Minimum optimal weight BMI
+        const double MINOVR = 30.0; //  Minimum overweight BMI
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            //  Declare and initialize global variables
+            int height = InputHeight();                 //  Set the height
+            int weight = InputWeight();                 //  Set the weight
+            double bmi = CalculateBMI(height, weight);  //   Calculate BMI
+            string bmiStatus = CalculateBMIStatus(bmi); //   Calculate BMI status
+            DisplayAll(height, weight,
+                       bmi, bmiStatus);                 //  Display all I/O values
+        }
+
+        private int InputHeight()
+        {
+            //  IsNumeric test variable
+            bool test = IsNumeric(textBoxHeight.Text);
+
+            //  Numeric test returned false, set to default height
+            if (!test)
+            {
+                return DEFHEIGHT;
+            }
+
+            //  Number entered for height.  Validate number
+            int h = Convert.ToInt32(textBoxHeight.Text);
+
+            //  Valid range check
+            if ((h < MINHEIGHT) || (h > MAXHEIGHT))
+            {
+                h = DEFHEIGHT;
+            }
+
+            return h;
+        }
+
+        private int InputWeight()
+        {
+            //  IsNumeric test variable
+            bool test = IsNumeric(textBoxWeight.Text);
+
+            //  Numeric test returned false, set to default weight
+            if (!test)
+            {
+                return DEFWEIGHT;
+            }
+
+            //  Number entered for height.  Validate number
+            int w = Convert.ToInt32(textBoxWeight.Text);
+
+            //  Valid range check
+            if ((w < MINWEIGHT) || (w > MAXWEIGHT))
+            {
+                w = DEFWEIGHT;
+            }
+
+            return w;
+        }
+
+        private bool IsNumeric(string input)
+        {
+            int test = 0;
+
+            return int.TryParse(input, out test);
+        }
+
+        private double CalculateBMI(int height, int weight)
+        {
+            return ((weight / (Math.Pow(height, 2))) * 703.0);
+        }
+
+        private string CalculateBMIStatus(double bmi)
+        {
+            string weightStr = "";
+
+            if ((bmi >= 0 ) && (bmi < MINOPT))
+            {
+                weightStr = "Underweight";
+            }
+            else if ((bmi >= MINOPT) && (bmi < MINOVR))
+            {
+                weightStr = "Optimal weight";
+            }
+            else if (bmi >= MINOVR)
+            {
+                weightStr = "Overweight";
+            }
+            else
+            {
+                weightStr = "Error Occurred During Processing";
+            }
+
+            return weightStr;
+        }
+
+        private void DisplayAll(int height, int weight, 
+                                double bmi,
+                                string bmiStatus)
+        {
+            textBoxHeight.Text      = height.ToString("n2");
+            textBoxWeight.Text      = weight.ToString("n2");
+            textBoxBMI.Text         = bmi.ToString("n2");
+            textBoxBMIStatus.Text   = bmiStatus;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            textBoxHeight.Text      = "";
+            textBoxWeight.Text      = "";
+            textBoxBMI.Text         = "";
+            textBoxBMIStatus.Text   = "";
+            textBoxHeight.Focus();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Exit Program Now?!?!?",
+                "EXIT PROGRAM???",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
